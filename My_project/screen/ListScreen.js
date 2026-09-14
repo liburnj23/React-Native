3c00b910d877c697db42cb0e5575e00364da011b
liburnj23 <liburn.j@gmail.com>
2026-09-14 19:13:54 +0200

    lesson4

---
+++ My_project/screen/ListScreen.js
@@ -0,0 +1,17 @@
+import react from "react";
+import {View , Text , StyleSheet , FlatList} from 'react-native';
+
+const students = [
+    {name: 'Gerti' , surname: 'Calaj' , age: '13'},
+    {name: "Deon" , surname:'Beka' , age:'16'},
+    {name:'Amar' , surname:'Buzuku', age:'16'}
+];
+
+const ListScreen = () => {
+    return(
+        <Text>Hello1</Text>   
+     );
+}
+
+
+const styles
